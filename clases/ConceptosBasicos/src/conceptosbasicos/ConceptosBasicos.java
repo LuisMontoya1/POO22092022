142459dc4553887b5c27e79ae7ff656a20dc05c1
luismontoya1 <luismontoya319@aragon.unam.mx>
2022-02-24 12:17:16 -0600

    poroyecto conceptos basicos

---
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,11 @@
+package conceptosbasicos;
+
+public class ConceptosBasicos {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola de nuevo");
+        
+    }
+    
+}
