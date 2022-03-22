87e7768ecb25f79cfbf45d7ba07f4357aa068ce7
luismontoya1 <luismontoya319@aragon.unam.mx>
2022-03-22 13:21:52 -0600

    primer respaldo

---
+++ clases/ReutilizacionCodigo2209/src/ico/fes/herencia/polimorfismo/Animal.java
@@ -0,0 +1,30 @@
+package ico.fes.herencia.polimorfismo;
+
+public class Animal {
+    
+    private int numeroPatas;
+
+    public Animal() {
+    }
+
+    public Animal(int numeroPatas) {
+        this.numeroPatas = numeroPatas;
+    }
+
+    public int getNumeroPatas() {
+        return numeroPatas;
+    }
+
+    public void setNumeroPatas(int numeroPatas) {
+        this.numeroPatas = numeroPatas;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "numeroPatas=" + numeroPatas + '}';
+    }
+    public void emitirSonido(){
+        System.out.println("Emitir sonido...");
+}
+    
+}
--- clases/ReutilizacionCodigo2209/src/reutilizacioncodigo2209/ReutilizacionCodigo2209.java
+++ clases/ReutilizacionCodigo2209/src/reutilizacioncodigo2209/ReutilizacionCodigo2209.java
@@ -10,6 +10,9 @@ import ico.fes.componentes.Monitor;
 import ico.fes.componentes.Procesador;
 import ico.fes.componentes.Teclado;
 import ico.fes.herencia.Alumno;
+import ico.fes.herencia.Servidor;
+import ico.fes.herencia.polimorfismo.Animal;
+import ico.fes.herencia.polimorfismo.Perro;
 import javax.swing.JFrame;
 
 /**
@@ -28,41 +31,59 @@ public class ReutilizacionCodigo2209 {
         System.out.println(nombre);
         System.out.println(nombre.charAt(3));
         System.out.println(nombre.toUpperCase());
-        
+
         /*JFrame ventana = new JFrame("Mi primer ventana Java");
         ventana.setSize(600, 400);
         ventana.setVisible(true);
-        */
-        
-        Computadora miCompu= new Computadora();
+         */
+        Computadora miCompu = new Computadora();
         miCompu.setMarca("Asus");
         miCompu.setCpu(new Procesador("Intel", 2.6f));
-        System.out.println( miCompu );
+        System.out.println(miCompu);
         miCompu.getCpu().setMarca("AMD");
-        System.out.println( miCompu );
+        System.out.println(miCompu);
         //Ejercicio: Establecer un mause marca logitech tipo optico
         miCompu.setRaton(new Mause("Logitech", "Óptico"));
         System.out.println(miCompu);
-        
-        Computadora compu2 = new Computadora("Apple", "MacBook Pro", 
+
+        Computadora compu2 = new Computadora("Apple", "MacBook Pro",
                 new Monitor("Toshiba", 14.3f),
-                new Mause("Acteck", "Optico"), 
-                new Teclado("Apple", 101), 
+                new Mause("Acteck", "Optico"),
+                new Teclado("Apple", 101),
                 new Procesador("M1", 3.4f));
         System.out.println(compu2);
         // mause apple y tipo touch
-         //compu2.setRaton(new Mause("Apple", "Touch"));
-         compu2.getRaton().setMarca("Apple");
-         compu2.getRaton().setTipo("Touch");
-         System.out.println(compu2);
-         
-         System.out.println("----------------------------------");
-         Alumno alu1= new Alumno();
-         alu1.setNombre("Jose");
-         System.out.println(alu1);
-         
-         Alumno alu2 = new Alumno("123456789", "ICO", "Jose Perez", 19);
-         System.out.println(alu2);
+        //compu2.setRaton(new Mause("Apple", "Touch"));
+        compu2.getRaton().setMarca("Apple");
+        compu2.getRaton().setTipo("Touch");
+        System.out.println(compu2);
+
+        System.out.println("----------------------------------");
+        Alumno alu1 = new Alumno();
+        alu1.setNombre("Jose");
+        System.out.println(alu1);
+
+        Alumno alu2 = new Alumno("123456789", "ICO", "Jose Perez", 19);
+        System.out.println(alu2);
+        
+        System.out.println("-------- Composicion y herencia en la misma clase -------");
+        Servidor server = new Servidor();
+        System.out.println(server);
+        server.setNumeroTarjetasRed(2);
+        System.out.println(server);
+        server.setMarca("HP");
+        System.out.println(server);
+        server.setRaton(new Mause ("LG", "Optico"));
+        System.out.println(server);
+        server.getRaton().setMarca("Logitech");
+        System.out.println(server);
+
+        System.out.println("------- Ejemplo polimorfismo --------");
+        Perro dog = new Perro ("Bull dog", "5 estrellas", 4);
+        Animal animal1 = new Animal(4);
+        dog.emitirSonido();
+        animal1.emitirSonido();
+        
     }
 
 }
