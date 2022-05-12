dc5f4ebf2cfcb79b78a41be335600359d6da8b1e
luismontoya1 <luismontoya319@aragon.unam.mx>
2022-05-12 13:54:50 -0500

    Ventana celcius

--- clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
+++ clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -2,12 +2,14 @@ package javabasico2209parte2;
 
 import ico.fes.awt.Ventana;
 import ico.fes.awt.VentanaVersion2;
+import ico.fes.swing.VentanaSwing;
 
 public class JavaGraficoMain {
     public static void main(String[] args) {
         
         // Ventana ventana = new Ventana ("Hola AWT ICO FES aragon");
         
-        VentanaVersion2 ventana =  new VentanaVersion2();
+        VentanaSwing vs = new VentanaSwing();
+        System.out.println(System.getProperty("user.dir"));
     }
 }
