c6d8a808018302be2fc20fb27a18b26152a251ce
luismontoya1 <luismontoya319@aragon.unam.mx>
2022-05-17 09:36:26 -0500

    Cifrado Cesar 2

--- tareas/CifradoCesar/src/cifradocesar/Ventana.java
+++ tareas/CifradoCesar/src/cifradocesar/Ventana.java
@@ -3,9 +3,7 @@ package cifradocesar;
 public class Ventana {
     public static void main(String[] args) {
         
-        // Ventana ventana = new Ventana ("Hola AWT ICO FES aragon");
-        
         CifradoCesar vs = new CifradoCesar();
-        System.out.println(System.getProperty("user.dir"));
+        
     }
 }
